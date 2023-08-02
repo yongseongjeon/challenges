9a2f0eb2b812bf24c6fce871e7a01342b4a8acca
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-02 17:32:16 +0900

    feat: Colon 컴포넌트 생성

---
+++ 2-digital-clock/src/components/Colon/Colon.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { Color } from "../../style/color";
+import { useEffect, useState } from "react";
+
+function Colon() {
+  const [isBlink, setIsBlink] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsBlink(!isBlink);
+    }, 1000);
+  }, [isBlink]);
+
+  return (
+    <Container>
+      <Circle isBlink={isBlink} />
+      <Circle isBlink={isBlink} />
+    </Container>
+  );
+}
+
+export default Colon;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  position: relative;
+  top: 46px;
+  right: 36px;
+`;
+
+const Circle = styled.div<{ isBlink: boolean }>`
+  width: 1rem;
+  height: 1rem;
+  border: 1px solid ${Color.BLACK};
+  border-radius: 10px;
+  background-color: ${({ isBlink }) => (isBlink ? Color.GREEN : Color.WHITE)};
+`;
