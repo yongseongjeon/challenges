89a13ef1c03f74643067068d25e30ae0901f0ae6
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-06 11:27:07 +0900

    refactor: 깜빡임 구현 setTimeout에서 animation으로 변경

--- 2-digital-clock/src/components/Colon/Colon.tsx
+++ 2-digital-clock/src/components/Colon/Colon.tsx
@@ -3,18 +3,10 @@ import { Color } from "../../style/color";
 import { useEffect, useState } from "react";
 
 function Colon() {
-  const [isBlink, setIsBlink] = useState<boolean>(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsBlink(!isBlink);
-    }, 1000);
-  }, [isBlink]);
-
   return (
     <Container>
-      <Circle $isBlink={isBlink} />
-      <Circle $isBlink={isBlink} />
+      <Circle />
+      <Circle />
     </Container>
   );
 }
@@ -30,10 +22,20 @@ const Container = styled.div`
   right: 36px;
 `;
 
-const Circle = styled.div<{ $isBlink: boolean }>`
+const Circle = styled.div`
   width: 1rem;
   height: 1rem;
   border: 1px solid ${Color.BLACK};
   border-radius: 10px;
-  background-color: ${({ $isBlink }) => ($isBlink ? Color.GREEN : Color.WHITE)};
+
+  @keyframes blink {
+    0%,
+    100% {
+      background-color: ${Color.WHITE};
+    }
+    50% {
+      background-color: ${Color.GREEN};
+    }
+  }
+  animation: blink 2s steps(1) infinite;
 `;
