d1624d08ddef540ad8c42bf38d739ada7f1fa560
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-08 15:44:13 +0900

    feat: GlobalStyle 적용

--- 4-presentation/src/App.tsx
+++ 4-presentation/src/App.tsx
@@ -1,6 +1,11 @@
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
-
