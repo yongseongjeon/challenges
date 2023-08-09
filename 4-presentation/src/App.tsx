1530032acaa69a33beaa7f8bd51527137f0206d7
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-09 23:32:26 +0900

    feat: Recoil 설치

--- 4-presentation/src/App.tsx
+++ 4-presentation/src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import { RouteConfig, routeConfigs } from "./route";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          {routeConfigs.map(({ id, path, element }: RouteConfig) => (
-            <Route key={id} path={path} element={element} />
-          ))}
-        </Routes>
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            {routeConfigs.map(({ id, path, element }: RouteConfig) => (
+              <Route key={id} path={path} element={element} />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
     </>
   );
 }
