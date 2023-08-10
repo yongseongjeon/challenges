fdf4f26f50f20ed4d080beb03b8c80bf536dfcaa
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-10 15:25:06 +0900

    feat: Carousel 좌우로 사진 이동 기능 추가

--- 4-presentation/src/components/Carousel/Carousel.tsx
+++ 4-presentation/src/components/Carousel/Carousel.tsx
@@ -4,25 +4,55 @@ import { useRecoilValue } from "recoil";
 import { picturesState } from "../../store/pictures";
 import { Color } from "../../styles/color";
 import { CommonStyle } from "../../styles/commonStyle";
+import { useState } from "react";
+
+const PICTURE_SIZE = "480";
 
 function Carousel() {
   const pictures = useRecoilValue(picturesState);
-
   const srcs = useFileReader({ files: pictures });
+  const [curPictureIdx, setCurPictureIdx] = useState(0);
+  const [isMoving, setIsMoving] = useState(false);
+  const lastIndex = srcs.length - 1;
 
   return (
     <CarouselWrapper>
-      <ArrowLeftButton />
+      <ArrowLeftButton onClick={handleClickArrowLeft} disabled={isMoving} />
       <PicturesWrapper>
-        <Pictures>
+        <Pictures
+          idx={curPictureIdx}
+          onTransitionEnd={() => {
+            setIsMoving(false);
+          }}
+        >
           {srcs.map((src) => {
             return <Img src={src} alt="유저가 업로드한 사진입니다." />;
           })}
         </Pictures>
       </PicturesWrapper>
-      <ArrowRightButton />
+      <ArrowRightButton onClick={handleClickArrowRight} disabled={isMoving} />
     </CarouselWrapper>
   );
+
+  function handleClickArrowLeft() {
+    setIsMoving(true);
+    const isFirstIndex = curPictureIdx === 0;
+    if (isFirstIndex) {
+      setCurPictureIdx(lastIndex);
+      return;
+    }
+    setCurPictureIdx(curPictureIdx - 1);
+  }
+
+  function handleClickArrowRight() {
+    setIsMoving(true);
+    const isLastIndex = curPictureIdx === lastIndex;
+    if (isLastIndex) {
+      setCurPictureIdx(0);
+      return;
+    }
+    setCurPictureIdx(curPictureIdx + 1);
+  }
 }
 
 export default Carousel;
@@ -32,7 +62,7 @@ const CarouselWrapper = styled.div`
   gap: 2rem;
 `;
 
-const ArrowLeftButton = styled.div`
+const ArrowLeftButton = styled.button`
   width: 0;
   height: 0;
   border-top: 2rem solid transparent;
@@ -45,7 +75,7 @@ const ArrowLeftButton = styled.div`
   }
 `;
 
-const ArrowRightButton = styled.div`
+const ArrowRightButton = styled.button`
   width: 0;
   height: 0;
   border-top: 2rem solid transparent;
@@ -71,6 +101,8 @@ const PicturesWrapper = styled.div`
   overflow: hidden;
 `;
 
-const Pictures = styled.div`
+const Pictures = styled.div<{ idx: number }>`
   display: flex;
+  transform: ${({ idx }) => `translateX(${-PICTURE_SIZE * idx}px)`};
+  transition: 0.25s;
 `;
