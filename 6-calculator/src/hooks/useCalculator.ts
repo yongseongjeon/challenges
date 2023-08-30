dcd2d649c89c547be68f338ce165aed2b3a2f5d0
yongseongjeon <yongseong.jeon95@gmail.com>
2023-08-30 22:29:09 +0900

    fix: 연산자로 끝나는 경우 계산 방지

--- 6-calculator/src/hooks/useCalculator.ts
+++ 6-calculator/src/hooks/useCalculator.ts
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { convertToPostfixNotation } from "./utils/postfixNotation/postfixNotation";
-import { calculatePostfixNotation } from "./utils/calculatorPostfixNotation/calculatorPostfixNotation";
+import { convertToPostfixNotation, isOperator } from "../utils/postfixNotation/postfixNotation";
+import { calculatePostfixNotation } from "../utils/calculatorPostfixNotation/calculatorPostfixNotation";
 
 function useCalculator(infixNotation: string[]) {
   const [result, setResult] = useState("");
 
   useEffect(() => {
+    const lastElement = infixNotation.at(-1);
+    if (isOperator(lastElement)) return;
     const postfixNotation = convertToPostfixNotation(infixNotation);
     const result = calculatePostfixNotation(postfixNotation);
     setResult(result);
