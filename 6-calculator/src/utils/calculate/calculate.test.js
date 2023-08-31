d9c59f81db73e2c465377fd9ee4927f87d172c35
yongseongjeon <yongseong.jeon95@gmail.com>
2023-09-01 00:35:36 +0900

    refactor: calculatorPostfixNotatin.js 확장자 ts로 변경

--- 6-calculator/src/utils/calculate/calculate.test.js
+++ 6-calculator/src/utils/calculate/calculate.test.js
@@ -3,18 +3,18 @@ import calculate from "./calculate";
 describe("중위표현식을 계산하면", () => {
   it("1 + 2 는 3 이다.", () => {
     const answer = calculate(["1", "+", "2"]);
-    expect(answer).toBe(3);
+    expect(answer).toBe("3");
   });
   it("1 - 2 * 3 는 -5 다.", () => {
     const answer = calculate(["1", "-", "2", "*", "3"]);
-    expect(answer).toBe(-5);
+    expect(answer).toBe("-5");
   });
   it("2 * 3 + 4 는 10 다.", () => {
     const answer = calculate(["2", "*", "3", "+", "4"]);
-    expect(answer).toBe(10);
+    expect(answer).toBe("10");
   });
   it("33 + 67 는 100 이다.", () => {
     const answer = calculate(["33", "+", "67"]);
-    expect(answer).toBe(100);
+    expect(answer).toBe("100");
   });
 });
---
+++ 6-calculator/src/utils/calculatorPostfixNotation/calculatorPostfixNotation.ts
@@ -0,0 +1,43 @@
+import { NotationElement, Operand, Operator } from "../../type";
+import { isOperator } from "../postfixNotation/postfixNotation";
+
+export function calculatePostfixNotation(postfixNotation: NotationElement[]) {
+  const postfixNotationList = postfixNotation.reverse();
+  const s = [postfixNotationList.pop()] as NotationElement[];
+  while (postfixNotationList.length > 0) {
+    const cur = postfixNotationList.pop() as Operator;
+    if (isOperator(cur)) {
+      const right = s.pop() as Operand;
+      const left = s.pop() as Operand;
+      const result = calculateOperations(left, right, cur);
+      s.push(String(result));
+    } else {
+      s.push(cur);
+    }
+  }
+  return s[0];
+}
+
+function calculateOperations(left: Operand, right: Operand, operator: Operator): number | Error {
+  const leftTerm = +left;
+  const rightTerm = +right;
+  if (operator === "+") {
+    return leftTerm + rightTerm;
+  }
+  if (operator === "-") {
+    return leftTerm - rightTerm;
+  }
+  if (operator === "*") {
+    return leftTerm * rightTerm;
+  }
+  if (operator === "/") {
+    if (rightTerm === 0) {
+      return new Error("0 으로 나눌 수 없습니다.");
+    }
+    return leftTerm / rightTerm;
+  }
+  if (operator === "%") {
+    return leftTerm % rightTerm;
+  }
+  return new Error(`${operator}는 올바른 연산자가 아닙니다.`);
+}
